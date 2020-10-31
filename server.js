67772489ad637305dca2d57e6e356407c29ddfd7
andypike7 <tech@sym.ru>
2020-10-31 11:34:50 +0300

    Base and logger

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./libs/log')/*(module)*/;
+const app = express();
+
+const PORT = 3333;
+
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public'))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+
+app.get('/api', (req, resp) => {
+  console.log('*** API is requested!');
+});
+
+app.use(function(req, res, next){
+  logger.log({
+    level: 'error',
+    message: `URL not regignized: ${req.url}`,
+    additional: 'properties',
+  });
+  res.status(404);
+  // res.send({ error: 'Not found' });
+  // return;
+});
+
+/*
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  logger.log({
+    level: 'debug',
+    message: `Internal error ({res.statusCode}): ${err.message}`);
+  res.send({ error: err.message });
+  return;
+});
+
+app.get('/ErrorExample', function(req, res, next){
+  next(new Error('Random error!'));
+});
+*/
+
+app.listen(PORT, () => {
+  logger.log({
+    level: 'info',
+    message: `*** Listening on port ${PORT}.`,
+    additional: 'properties',
+  });
+});
